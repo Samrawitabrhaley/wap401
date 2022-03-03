167d892bdf9c7c494af66f8e9e728dc6fde750c6
Samrawitabrhaley <100261216+Samrawitabrhaley@users.noreply.github.com>
2022-03-03 00:21:30 -0600

    hi

--- lectures/app.js
+++ lectures/app.js
@@ -1,10 +1,31 @@
-console.log("Hello");
-var address = { 
-	street: 'Main Street', 
-	'number': 1000, 
-	apartment: { 
-		'floor': 3, 
-		'number': 301 
-	} 
-    }
-console.log(address);
+"use strict";
+console.log("this is js");
+
+const test = () => {
+	const para=document.getElementById("dave");
+	console.log(para);
+	//para.style.color="red";
+	para.className="myclass";
+	const butn=document.getElementById("aa");
+	butn.onclick=disp;
+	setInterval(intr,1000);
+
+}
+const intr= () =>{
+	const para=document.getElementById("dave");
+	const today = new Date();
+	const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+ ' '+  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    para.innerHTML=date;
+}
+const disp = () => {
+	alert("ok");
+}
+
+var names=[2,2,4];
+const reduce = (x, y) => x + y
+
+console.log(names.reduce(reduce,0));
+
+window.onload=test
+
+
